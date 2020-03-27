7f2e1334da70baae2f10e6391b27058fc07c2f2b
igor <igor_paiva21@live.com>
2020-03-27 18:15:04 -0300

    background

---
+++ build/precache-manifest.d74be7b4475fa563e384047c1407dab4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b175fc829a466938df179f732797896",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71c276a0ab67859c202c",
+    "url": "/static/css/main.0b419204.chunk.css"
+  },
+  {
+    "revision": "07ed22df556094169b13",
+    "url": "/static/js/2.0ed9bd15.chunk.js"
+  },
+  {
+    "revision": "5d65e6d00793b56741884a22f660d870",
+    "url": "/static/js/2.0ed9bd15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71c276a0ab67859c202c",
+    "url": "/static/js/main.f408f57c.chunk.js"
+  },
+  {
+    "revision": "95b1865cda851fbf10c2",
+    "url": "/static/js/runtime-main.77bd7300.js"
+  },
+  {
+    "revision": "c67d34196da859e0e4e74cb1e5c27b05",
+    "url": "/static/media/logo.c67d3419.svg"
+  }
+]);
